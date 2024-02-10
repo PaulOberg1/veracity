7be694a9ead524d53cee00d46485194d23761424
PaulOberg1 <pauloberg1931@gmail.com>
2024-02-10 21:22:06 +0000

    Feature: Implement URL to video ID converting function
    Define function to convert a given URL to video ID
    
    Reduce server load and thus improve performance

--- client/src/components/ratingPage.js
+++ client/src/components/ratingPage.js
@@ -8,6 +8,21 @@ import React, {useState} from "react";
 import axios from "axios";
 
 
+/**
+ * Decodes a given video URL into video ID
+ * @param {string} url The video URL
+ * @returns {string | null} The video ID
+ */
+const getVideoID = (url) => {
+    const string = "/^.*(youtu\.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/"; //String to compare against
+    const id = url.match(string); //Matches string with given url
+    
+    if (match && match[2].length === 11) { //If output valid, ID of expected length
+        return match[2];
+    } else {
+        return null;
+    }
+}
 
 
 const ratingPage = () => {
@@ -21,8 +36,8 @@ const ratingPage = () => {
      *  Updates 'comments' state
      */
     const getComments = () => {
-        const url = window.location.href;
-        axios.post("/getComments",url)
+        const videoID = getVideoID(window.location.href);
+        axios.post("/getComments",videoID)
         .then(response => response.json)
         .then(result => {
             console.log(`success : ${result.message}`);
