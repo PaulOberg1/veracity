3f2827c435e604861fea62bb013c79732d530b6d
PaulOberg1 <pauloberg1931@gmail.com>
2024-02-10 19:19:22 +0000

    Feature: Add video-rating functionality
    Send comments to backend and receive sentiment analysis score

--- client/src/components/ratingPage.js
+++ client/src/components/ratingPage.js
@@ -2,9 +2,27 @@ import react, {useState} from "react";
 import axios from "axios";
 
 const ratingPage = () => {
+    const [comments,setComments] = useState("");
+    const [rating, setRating] = useState(null);
+    
+    const sendComments = () => {
+        axios.post("/rate",comments)
+        .then(response => response.json)
+        .then(result => {
+            console.log("response: " + result.message);
+            setRating(result.data);
+        })
+        .catch((error) => {
+            console.error("error : " + error.message)
+        })
+    }
+
     return (
         <div>
-            
+            <button onClick={() => {sendComments}}>Veracity Rating</button>
+            {rating &&
+            <p>Rating = {rating}</p>
+            }
         </div>
     )
 }
