f49b003a168bf3d9511ed9f034fa715860722a0a
PaulOberg1 <pauloberg1931@gmail.com>
2024-02-10 21:53:44 +0000

    Feature: Add basic sentiment analysis functionality
    Define function to convert an array of comments to a corresponding rating
    
    Convert rating to intuitive form - e.g. 'neg' -> 'Negative'

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from googleapiclient.discovery import build
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 app = Flask(__name__)
 youtube = build("youtube","v3",developerKey="AIzaSyAO7SrY7EwY2JHv28qksxr2xmOSwbA96PU")
@@ -32,3 +33,23 @@ def getComments():
     except Exception as e:
         return jsonify({"message":f"error: {e}"}),500
 
+
+@app.route("/rate",methods=["POST"])
+def rate():
+    """
+    Perform sentiment analysis on a set of comments and return a score
+
+    Returns:
+    - JSON: Data field contains sentiment analysis score as a string
+    """
+    try:
+        comments = request.json
+        analyzer = SentimentIntensityAnalyzer()
+        text = sum(comments) #Concatenate all the comments into a single block of text
+        scores = analyzer.polarity_scores(text) #Compute the sentiment analysis scores of the text
+        codeToWord = {"neg":"Negative","neu":"Neutral","pos":"Positive","compound":"Compound"} #Maps keys in 'scores' variable to readable form
+        result = codeToWord[max([(key,value) for key,value in scores.items()],lambda pair: pair[1])[0]] #Obtains final rating as adjective
+
+        return jsonify({"message":"success","data":result}),200
+    except Exception as e:
+        return jsonify({"message":f"error: {e}"}),500
