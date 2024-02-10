a69f84ffa60d5b80e3c18d4e51f351f569ab9059
PaulOberg1 <pauloberg1931@gmail.com>
2024-02-10 19:49:49 +0000

    Feature: Implement basic summary page structure
    Send transcript to backend and receive summarised form
    
    Display button to summarise transcript
    
    Display summarised transcript

--- client/src/components/summaryPage.js
+++ client/src/components/summaryPage.js
@@ -1,10 +1,44 @@
+/*
+    File: summaryPage.js
+    Description: Implement transcript summary feature.
+    Dependencies: react, axios 
+*/
+
 import react, {useState} from "react";
 import axios from "axios";
 
+
+
 const summaryPage = () => {
+    const [transcript,setTranscript] = useState("");
+    const [summaryHTML,setSummaryHTML] = useState(null);
+
+    /*
+        Makes POST request to backend sending transcript data
+        Receives summary HTML data from backend and stores data
+        Handles error checking
+    */
+    const sendTranscript = () => {
+        axios.post("/summarise",transcript)
+        .then(response => response.json)
+        .then(result => {
+            console.log("response : " + result.message);
+            setSummaryHTML(result.text); //Store summary data as html on frontend
+        })
+        .catch((error) => {
+            console.error("error: " + e);
+        })
+    }
+
     return (
         <div>
-
+            {transcript && (
+                <button onClick={sendTranscript}>Summarise Video</button>
+            )}
+            {summaryHTML && (
+                <div dangerouslySetInnerHTML={{__html: summaryHTML}} /> 
+            )}
+            
         </div>
     )
 }
