480e35a1f7f341fe4dafdfb34b524b185fbaebe8
PaulOberg1 <pauloberg1931@gmail.com>
2024-02-11 15:54:59 +0000

    Bugfix: Fix errors in App.js
    Modify import line to import BrowserRouter
    
    Pass React element to element props of <Route> component instead of objects

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
 import './App.css';
-import {Router,Routes,Route,Navigate} from "react-router-dom";
+import {BrowserRouter as Router,Routes,Route,Navigate} from "react-router-dom";
+import HomePage from "./components/HomePage.js";
+import RatingPage from "./components/RatingPage.js";
+import SummaryPage from "./components/SummaryPage.js";
 
 function App() {
   return (
     <Router>
       <div>
         <Routes>
-          <Route path="/rate" element={{RatingPage}}/>
-          <Route path="/summarise" element={{SummaryPage}}/>
-          <Route path="/home" element={{HomePage}}/>
-          <Route path="/" element={
-            <>
-              <Navigate to="/home"/>
-            </>
-          }/>
+          <Route path="/rate" element={<RatingPage />}/>
+          <Route path="/summarise" element={<SummaryPage />}/>
+          <Route path="/home" element={<HomePage />}/>
+          <Route path="/" element={<Navigate to="/home"/>}/>
         </Routes>
       </div>
     </Router>
