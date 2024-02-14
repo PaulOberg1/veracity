cd28f3ab11450541d5033643c18af4ec2291c56b
PaulOberg1 <pauloberg1931@gmail.com>
2024-02-14 13:16:12 +0000

    Refactor: Combine Flask routes into /getMetadata Removed /getDescription and /getComments routes in backend Combined functionality into /getMetadata route Removes redundant code, maintains cleanliness

--- flask-server/server.py
+++ flask-server/server.py
@@ -3,9 +3,13 @@ from googleapiclient.discovery import build
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 from flask_cors import CORS
 import logging
+import requests
+from summary import generate_summary
+
 
 app = Flask(__name__)
 CORS(app)
+app.config["PREFERRED_URL_SCHEME"]="https"
 youtube = build("youtube","v3",developerKey="AIzaSyDk0We7u9xaFhJuJnJKIPJRbox0Km6X3n0")
 
 #Configure logger
@@ -13,72 +17,6 @@ file_handler = logging.FileHandler("app.log")
 file_handler.setLevel(logging.DEBUG)
 app.logger.addHandler(file_handler)
 app.logger.setLevel(logging.DEBUG)
-
-
-@app.route("/getComments", methods=["POST"])
-def getComments():
-    """
-    Fetch YouTube comments given a video ID
-
-    Input:
-    - JSON: A JSON object containing the video ID
-
-    Returns:
-    - JSON: A JSON object containing array of comments
-    """
-    
-    try:
-        app.logger.debug("Request received at /getComments")
-        videoID = request.json.get("videoID") #Fetch video ID from request data
-        app.logger.debug(f"Video ID: {videoID}")
-
-        #Fetch comments via YouTube API
-        response = youtube.commentThreads().list(
-            part="snippet",
-            videoId=videoID,
-            maxResults=100
-        ).execute()
-
-        #Store comments in array
-        comments = []
-        for item in response["items"]:
-            comment = item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
-            comments.append(comment)
-        
-        return jsonify({"message":"succesfully fetched comments","data":comments}),200
-    except Exception as e:
-        app.logger.error(f"Error with /getComments route: {e}")
-        return jsonify({"message":f"error: {e}"}),500
-
-@app.route("/getDescription",methods=["POST"])
-def getDescription():
-    """
-    Fetch transcript of a YouTube video given a video ID
-
-    Input:
-    - JSON: A JSON object containing the video ID
-
-    ReturnsL
-    - JSON: A JSON object containing the transcript if function successful
-    """
-    try:
-        app.logger.debug("Request received at /getDescription")
-        videoID = request.json.get("videoID") #Fetch video ID from request data
-        app.logger.debug(videoID)
-        response = youtube.videos().list( #
-            part="snippet",
-            id = videoID
-        ).execute()
-        if response:
-            app.logger.debug(response)
-            app.logger.debug("response successful")
-        video_description = response["items"][0]["snippet"]["description"]
-        app.logger.debug(f"\n\n\nDESCRIPTION = {video_description}")
-        
-        return jsonify({"message":"description successfully retrieved from YouTube API", "data":video_description}),200
-    except Exception as e:
-        app.logger.error(f"Error with /getDescription route: {e}")
-        return jsonify({"message":f"error: {e}"}),500
 
 @app.route("/rate",methods=["POST"])
 def rate():
@@ -93,7 +31,7 @@ def rate():
     """
     try:
         app.logger.debug("Received request at /rate")
-        comments = request.json
+        comments = request.json.get("metadata")["comments"]
         analyzer = SentimentIntensityAnalyzer()
         text = "".join(list(comments)) #Concatenate all the comments into a single block of text
         scores = analyzer.polarity_scores(text) #Compute the sentiment analysis scores of the text
@@ -103,6 +41,85 @@ def rate():
     except Exception as e:
         app.logger.error(f"Error with /rate route: {e}")
         return jsonify({"message":f"error: {e}"}),500
+
+    
+
+@app.route("/getMetadata", methods=["POST"])
+def getMetadata():
+    """
+    Fetch metadata of video given its ID
+    Request video's title, description, comments and category from YouTube Data API
+
+    Input:
+    - JSON: A JSON object containing the video ID
+
+    Returns
+    - JSON: A JSON object containing video's metadata
+    """
+    try:
+        app.logger.debug("successfully accessed /getMetadata endpoint")
+        videoID = request.json.get("videoID") #Fetch video ID from request data
+
+        #Accessing video description, title and category
+        params = { #Parameters to be sent with request to YouTube Data API
+            "part":"snippet",
+            "id":videoID,
+            "maxResults":100, 
+            "key":"AIzaSyDk0We7u9xaFhJuJnJKIPJRbox0Km6X3n0" #API key
+        }
+        url = "https://www.googleapis.com/youtube/v3/videos" #URL to request data from
+        data = requests.get(url,params=params).json()
+
+        description = data["items"][0]["snippet"]["description"]
+        title = data["items"][0]["snippet"]["title"]
+        category = data["items"][0]["snippet"].get("categoryID")
+
+        #Accessing comments section
+        params = {
+            "part":"snippet",
+            "videoId":videoID,
+            "key":"AIzaSyDk0We7u9xaFhJuJnJKIPJRbox0Km6X3n0"
+        }
+        url = "https://www.googleapis.com/youtube/v3/commentThreads"
+        data = requests.get(url,params=params).json()
+
+        comments = []
+        for item in data["items"]: #Store comments in array
+            comment = item["snippet"]["topLevelComment"]["snippet"]["textDisplay"]
+            comments.append(comment)
+
+        
+        #Accesing channel name
+        channel_id = data["items"][0]["snippet"]["channelId"]
+        url = "https://www.googleapis.com/youtube/v3/channels"
+        params = {
+            "part":"snippet",
+            "id":channel_id,
+            "key":"AIzaSyDk0We7u9xaFhJuJnJKIPJRbox0Km6X3n0"
+        }
+        data = requests.get(url,params=params).json()
+        
+        channel_title = data["items"][0]["snippet"]["title"]
+        channel_description = data["items"][0]["snippet"]["description"]
+        channel_creation_date = data["items"][0]["snippet"]["publishedAt"]
+        channel_thumbnail = data["items"][0]["snippet"]["thumbnails"].get("default",{}).get("url")
+        
+        return jsonify({"message":"successfully retrieved metadata",
+                        "data": {
+                            "comments":comments,
+                            "description":description,
+                            "title":title,
+                            "category":category,
+                            "channel_title":channel_title,
+                            "channel_description":channel_description,
+                            "channel_creation_date":channel_creation_date,
+                            "channel_thumbnail":channel_thumbnail
+                        }
+                        })
+    except Exception as e:
+        app.logger.error(f"failure at /getMetadata endpoint: {e}")
+        return jsonify({"message":f"unsuccessful retrieving metadata: {e}"}),500
+
 
 @app.route('/') #Root of application
 def index():
